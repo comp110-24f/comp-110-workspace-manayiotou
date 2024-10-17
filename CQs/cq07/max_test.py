9fe3db7510f1070c6424f9aa2e4e3db61a0380d9
Maria Anayiotou <mariaan@unc.edu>
2024-10-16 22:09:20 -0400

    cq

--- CQs/cq07/max_test.py
+++ CQs/cq07/max_test.py
@@ -5,10 +5,18 @@ from CQs.cq07.find_max import find_and_remove_max
 __author__ = "730656009"
 
 
-def test_find_and_remove_max() -> None:
+def test_find_and_remove_max_return_value() -> None:
     input = [1, 13, 4, 7]
     assert find_and_remove_max(input) == 13
-    assert find_and_remove_max(input) == [1, 4, 7]
 
+
+def test_find_and_remove_max_mutated_list() -> None:
+    input = [4, 83, 22, 12]
+    find_and_remove_max(input)
+    assert find_and_remove_max(input) == [4, 22, 12]
+
+
+def test_find_and_remove_max_empty_list() -> None:
     input = []
     assert find_and_remove_max(input) == -1
+    assert input == []
