524a9bb27c8a4f596dd452dc99ad8b5a4ca5f13d
Maria Anayiotou <mariaan@unc.edu>
2024-10-21 19:54:40 -0400

    ex05

--- exercises/ex05.py/utils.py
+++ exercises/ex05.py/utils.py
@@ -4,12 +4,12 @@ __author__ = "730656009"
 
 def only_evens(input: list[int]) -> list[int]:
     new: list[int]= []
-    for elem in input:
-        if elem % 2 == 0: 
-            new.append(elem)
+    for num in input:
+        if num % 2 == 0: 
+            new.append(num)
     return new
 
 def sub(input: list[int], start_i: int, end_i: int) -> list[int]:
-
+    
 
 def add_at_index(input: list[int], add_elem: int, given_i: int) -> None:
